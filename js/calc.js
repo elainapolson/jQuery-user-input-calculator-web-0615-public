929312f1f4025342513d35a9f526844369d55896
elainapolson <elainapolson@gmail.com>
2015-07-16 18:51:02 -0400

    done

--- js/calc.js
+++ js/calc.js
@@ -5,4 +5,24 @@ $(function(){
 
 });
 
-// your code here!
+function Calculator() {
+
+
+
+
+  $("#equals").click(function(e) {
+
+  var number1 = parseInt($('#number1').val());
+  var number2 = parseInt($('#number2').val());
+  var operator = $('#operation').val();
+  
+    if (!($.isNumeric(number1)) || !($.isNumeric(number2))) {
+      $('#result').text('Sorry, one of those is not a valid number!');
+    } else if (operator != "+" && operator != "-" && operator != "*" && operator != "/" && operator != "%" ) {
+      $('#result').text('Sorry, not a valid operation!');
+    } else {
+      $('#result').text(eval(number1 + operator + number2));
+    }
+  });
+
+}
